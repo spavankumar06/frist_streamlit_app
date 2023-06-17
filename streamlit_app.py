d9be11dd21f691dbf1fa1a80b7e20cf1820ac09d
spavankumar06 <55975734+spavankumar06@users.noreply.github.com>
2023-06-17 21:46:28 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header(🥣 'Breakfast Menu')
+streamlit.text(🥗 'Omega 3 & Blueberry Oatmeal')
+streamlit.text( 🐔 'Kale, Spinach & Rocket Smoothie')
+streamlit.text( 🥑🍞 'Hard-Boiled Free-Range Egg')
 
-streamlit.header('Suprabhatam')
-
-streamlit.text('Shubhodaya')
-
-streamlit.text('Bharat Mata ki Jai')
-
+ 
